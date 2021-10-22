fe95dc7b7bf7439a5b64a8bd2833a52362633811
hks1 <hks1@users.noreply.github.com>
2021-10-22 09:46:42 -0400

    containerize it (docker) - added method to accept endpoint "/"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping
+	public String home(){
+		return "Hello Docker World!";
 	}
 
 	@GetMapping("/hello")
